e0c6d1fecfbeee806ecda600834fe8535bb385da
Carolina de la Fuente <mdela160@fiu.edu>
2017-03-24 14:46:48 -0400

    Adventure Mode

--- public/main.js
+++ public/main.js
@@ -6,42 +6,48 @@ const main = () => {
   let lightnessValue = 50
   let alphaValue = 1
   const hueSlider = document.querySelector('input[name=hue]')
-  const hueNumber = document.querySelector('div.hueColor')
+  // const hueNumber = document.querySelector('div.hueColor')
+  const colorBar = document.querySelector('.colorBar')
   hueSlider.addEventListener('input', (event) => {
     hueValue = event.target.value
-    hueNumber.textContent = hueValue
+    // hueNumber.textContent = hueValue
     updateColor()
   })
 
   const saturationSlider = document.querySelector('input[name=saturation]')
-  const saturationNumber = document.querySelector('div.saturationColor')
+  // const saturationNumber = document.querySelector('div.saturationColor')
   saturationSlider.addEventListener('input', (event) => {
     saturationValue = event.target.value
-    saturationNumber.textContent = saturationValue + '%'
+    // saturationNumber.textContent = saturationValue + '%'
     updateColor()
   })
 
   const lightnessSlider = document.querySelector('input[name=lightness]')
-  const lightnessNumber = document.querySelector('div.lightnessColor')
+  // const lightnessNumber = document.querySelector('div.lightnessColor')
   lightnessSlider.addEventListener('input', (event) => {
     lightnessValue = event.target.value
-    lightnessNumber.textContent = lightnessValue + '%'
+    // lightnessNumber.textContent = lightnessValue + '%'
     updateColor()
   })
 
   const alphaSlider = document.querySelector('input[name=alpha]')
-  const alphaNumber = document.querySelector('div.alphaColor')
+  // const alphaNumber = document.querySelector('div.alphaColor')
   alphaSlider.addEventListener('input', (event) => {
     alphaValue = event.target.value / 100
-    alphaNumber.textContent = alphaValue
+    // alphaNumber.textContent = alphaValue
     updateColor()
   })
 
   const updateColor = () => {
-    let color = `hsla(${hueValue}, ${saturationValue}%, ${lightnessValue}%, ${alphaValue})`
+    let color
+    if (alphaValue < 1) {
+      color = `hsla(${hueValue}, ${saturationValue}%, ${lightnessValue}%, ${alphaValue})`
+    } else {
+      color = `hsl(${hueValue}, ${saturationValue}%, ${lightnessValue}%)`
+    }
     swatch.style.backgroundColor = color
+    colorBar.textContent = color
   }
-
   updateColor()
 }
 
