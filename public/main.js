14ebeac5469e801e174519ae4988757c2595667d
Carolina de la Fuente <mdela160@fiu.edu>
2017-03-26 21:05:25 -0400

    Final Color Picker

--- public/main.js
+++ public/main.js
@@ -1,43 +1,35 @@
 const main = () => {
   const swatch = document.querySelector('div.swatch')
-  // let hidden = document.querySelector('div.hidden')
   let hueValue = Math.round(Math.random() * 360)
   let saturationValue = 50
   let lightnessValue = 50
   let alphaValue = 1
+  // hue controls
   const hueSlider = document.querySelector('input[name=hue]')
-  // const hueNumber = document.querySelector('div.hueColor')
   const colorBar = document.querySelector('.colorBar')
   hueSlider.addEventListener('input', (event) => {
     hueValue = event.target.value
-    // hueNumber.textContent = hueValue
     updateColor()
   })
-
+// saturation controls
   const saturationSlider = document.querySelector('input[name=saturation]')
-  // const saturationNumber = document.querySelector('div.saturationColor')
   saturationSlider.addEventListener('input', (event) => {
     saturationValue = event.target.value
-    // saturationNumber.textContent = saturationValue + '%'
     updateColor()
   })
-
+// lightness controls
   const lightnessSlider = document.querySelector('input[name=lightness]')
-  // const lightnessNumber = document.querySelector('div.lightnessColor')
   lightnessSlider.addEventListener('input', (event) => {
     lightnessValue = event.target.value
-    // lightnessNumber.textContent = lightnessValue + '%'
     updateColor()
   })
-
+// alpha controls
   const alphaSlider = document.querySelector('input[name=alpha]')
-  // const alphaNumber = document.querySelector('div.alphaColor')
   alphaSlider.addEventListener('input', (event) => {
     alphaValue = event.target.value / 100
-    // alphaNumber.textContent = alphaValue
     updateColor()
   })
-
+// values function
   const updateColor = () => {
     let color
     if (alphaValue < 1) {
@@ -51,5 +43,4 @@ const main = () => {
   updateColor()
 }
 
-// document.querySelector('h1').textContent += '!'
 document.addEventListener('DOMContentLoaded', main)
